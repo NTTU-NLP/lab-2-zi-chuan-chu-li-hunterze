3457bd3634c2d96d34f781bc72d01b4355d93ced
hunterze <hunterze6162@gmail.com>
2017-03-06 16:49:50 +0800

    交作業

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"C:\Users\admin\Desktop\lab-2-zi-chuan-chu-li-hunterze\Data\Input.txt")) 
+            {
+
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        Console.Write(ch);
+                        if(ch=='?')
+                        {
+                            Console.Write('\n');
+                        }
+                        if (ch == '.')
+                        {
+                            Console.Write('\n');
+                        }
+                        if (ch == '!')
+                        {
+                            Console.Write('\n');
+                        }
+                    }
+                }
+                Console.Read();
+            }
+        }
+    }
+}
